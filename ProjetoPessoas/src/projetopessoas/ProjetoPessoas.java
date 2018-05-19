11b31fe6190fa6accaa8761f11b6b6f5137768b4
jwictor <39440505+jwictor@users.noreply.github.com>
2018-05-19 15:08:01 -0300

    Add files via upload

---
+++ ProjetoPessoas/src/projetopessoas/ProjetoPessoas.java
@@ -0,0 +1,36 @@
+
+package projetopessoas;
+
+
+public class ProjetoPessoas {
+
+   
+    public static void main(String[] args) {
+        // Programa Principal
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Claudia");
+        p4.setNome("Fabiana");
+        
+        p1.setSexo("M");
+        p4.setSexo("F");
+        p2.setSexo("H");
+        
+        p2.setCurso("Informatica");
+        p3.setSalario(2500.75f);
+        p4.setSetor("Estoque");
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        
+        
+    }
+    
+}
